91dd0617d6256a56ae18ff5c563b7120f80d990d
KOOMPI AI <keanglongky-del@users.noreply.github.com>
2026-06-15 17:25:54 +0700

    Mission logo: fit inside quarter-circle (w-10 md:w-16)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -260,7 +260,7 @@ export default function Home() {
         <div className="absolute top-0 left-0 w-20 md:w-36 h-20 md:h-36 rounded-br-[100%] bg-gradient-to-br from-[#1a3a1e]/70 to-[#142e18]/50 z-5">
           {/* Logo centered inside quarter-circle */}
           <div className="absolute inset-0 flex items-center justify-center">
-            <Image src="/sunrise-pp-logo.png" alt="Sunrise" width={120} height={135} className="w-14 md:w-24 h-auto opacity-90" />
+            <Image src="/sunrise-pp-logo.png" alt="Sunrise" width={80} height={90} className="w-10 md:w-16 h-auto opacity-90" />
           </div>
         </div>
 
