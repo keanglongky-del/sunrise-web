76ef24600c79e0bb69da555ef0fc88a1d82e8367
KOOMPI AI <keanglongky-del@users.noreply.github.com>
2026-06-24 15:03:56 +0700

    Upscale EU GI logo + increase cert logo display size to 120px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -608,7 +608,7 @@ export default function Home() {
               {/* Logo */}
               {cert.logo ? (
                 <div className="w-full flex-1 flex items-center justify-center mb-2 relative">
-                  <div className="relative w-3/4 h-3/4 max-w-[90px] max-h-[90px]">
+                  <div className="relative w-4/5 h-4/5 max-w-[120px] max-h-[120px]">
                     <Image src={cert.logo} alt={cert.name} fill className="object-contain" />
                   </div>
                 </div>
